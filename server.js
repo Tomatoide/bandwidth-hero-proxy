7f23444d70d9ad2244d8c37a38303dcb94717d43
Knight <6270814+Tomatoide@users.noreply.github.com>
2021-04-08 22:22:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ const params = require('./src/params')
 const proxy = require('./src/proxy')
 const spdy = require('spdy')
 const fs = require('fs')
+
+// ssl doesn't make sense when we have ssl termination.
+/*
 const ssl = {
     key: fs.readFileSync('./cert/privkey.pem'),
     cert: fs.readFileSync('./cert/fullchain.pem')
 }
-
+*/
+const ssl = false;
 const PORT = process.env.PORT || 8080
 
 app.enable('trust proxy')
 app.get('/favicon.ico', (req, res) => res.status(204).end())
-app.get('*', authenticate, params, proxy)
+app.get('/', authenticate, params, proxy)
 spdy.createServer(ssl, app).listen(PORT, () => console.log(`Listening on ${PORT}`))
